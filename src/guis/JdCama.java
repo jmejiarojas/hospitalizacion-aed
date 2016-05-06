14e49cebe10758d4511bca5f7ded3a2353bb81ed
jmejiarojas <julioedgarmejiarojas338@gmail.com>
2016-05-05 20:29:22 -0500

    Se agrega tabla a gui Camas
    -Se implementa una tabla para que se muestre las camas.
    -Se agrega 2 paneles para que de mejor aspecto a la gui.
    -Se implementa el metodo mostrarListado.
    -Se agrega el icono de exit.gif en nuestra carpeta de iconos.

--- src/guis/JdCama.java
+++ src/guis/JdCama.java
@@ -7,6 +7,7 @@ import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.table.DefaultTableModel;
 
 import controladores.Camas;
 import entidades.Cama;
@@ -21,11 +22,17 @@ import java.awt.Font;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import javax.swing.ImageIcon;
+import javax.swing.JTable;
 
 public class JdCama extends JDialog {
 	
 	//Variables globales
 	Camas camas = new Camas();
+	
+	//Para crear la tabla
+	private String[] cabecera = {"Número", "Categoría","Precio x día",  "Estado"};
+	private String[][] data = {};
+	private DefaultTableModel modeloCama;
 
 	private final JPanel contentPanel = new JPanel();
 	private JPanel panel;
@@ -42,10 +49,13 @@ public class JdCama extends JDialog {
 	private JLabel lblNewLabel_2;
 	private JLabel lblNewLabel_3;
 	private JLabel lblNewLabel_4;
-	private JTextField txtNumero;
 	private JTextField txtPrecio;
 	private JComboBox cboCategoria;
 	private JComboBox cboEstado;
+	private JTable tblCamas;
+	private JLabel lblCodigo;
+	private JPanel panel_1;
+	private JPanel panel_2;
 
 	/**
 	 * Launch the application.
@@ -65,7 +75,7 @@ public class JdCama extends JDialog {
 	 */
 	public JdCama() {
 		setTitle("MANTENIMIENTO DE CAMAS");
-		setBounds(100, 100, 612, 447);
+		setBounds(100, 100, 626, 463);
 		getContentPane().setLayout(new BorderLayout());
 		contentPanel.setBackground(new Color(220, 20, 60));
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
@@ -90,31 +100,33 @@ public class JdCama extends JDialog {
 		
 		lblNewLabel_3 = new JLabel("Categor\u00EDa");
 		lblNewLabel_3.setFont(new Font("Tahoma", Font.BOLD, 12));
-		lblNewLabel_3.setBounds(10, 110, 80, 14);
+		lblNewLabel_3.setBounds(10, 94, 80, 14);
 		panel.add(lblNewLabel_3);
 		
 		lblNewLabel_4 = new JLabel("Estado");
 		lblNewLabel_4.setFont(new Font("Tahoma", Font.BOLD, 12));
-		lblNewLabel_4.setBounds(230, 110, 46, 14);
+		lblNewLabel_4.setBounds(10, 132, 46, 14);
 		panel.add(lblNewLabel_4);
 		
-		txtNumero = new JTextField();
-		txtNumero.setBounds(127, 21, 86, 20);
-		panel.add(txtNumero);
-		txtNumero.setColumns(10);
-		
 		txtPrecio = new JTextField();
+		txtPrecio.setFont(new Font("Tahoma", Font.BOLD, 13));
 		txtPrecio.setBounds(127, 57, 86, 20);
 		panel.add(txtPrecio);
 		txtPrecio.setColumns(10);
 		
 		cboCategoria = new JComboBox();
-		cboCategoria.setBounds(100, 107, 100, 20);
+		cboCategoria.setBounds(127, 92, 100, 20);
 		panel.add(cboCategoria);
 		
 		cboEstado = new JComboBox();
-		cboEstado.setBounds(286, 107, 99, 20);
+		cboEstado.setBounds(127, 130, 99, 20);
 		panel.add(cboEstado);
+		
+		lblCodigo = new JLabel("xyz");
+		lblCodigo.setForeground(new Color(0, 0, 255));
+		lblCodigo.setFont(new Font("Tahoma", Font.BOLD, 15));
+		lblCodigo.setBounds(127, 23, 57, 14);
+		panel.add(lblCodigo);
 		
 		lblNewLabel = new JLabel("Mantenimiento de Camas");
 		lblNewLabel.setForeground(new Color(240, 255, 255));
@@ -122,57 +134,112 @@ public class JdCama extends JDialog {
 		lblNewLabel.setBounds(166, 30, 259, 14);
 		contentPanel.add(lblNewLabel);
 		
+		scrollPane = new JScrollPane();
+		scrollPane.setBounds(30, 279, 559, 135);
+		contentPanel.add(scrollPane);
+		
+		tblCamas = new JTable();
+		scrollPane.setViewportView(tblCamas);
+		
+		//Configurando modelo en caja
+		modeloCama = new DefaultTableModel(data, cabecera);
+		tblCamas.setModel(modeloCama);
+		
+		panel_1 = new JPanel();
+		panel_1.setBackground(new Color(173, 216, 230));
+		panel_1.setBounds(443, 55, 146, 217);
+		contentPanel.add(panel_1);
+		panel_1.setLayout(null);
+		
+		cboIngresar = new JButton("Agregar");
+		cboIngresar.setBounds(10, 52, 128, 32);
+		panel_1.add(cboIngresar);
+		cboIngresar.setIcon(new ImageIcon(JdCama.class.getResource("/iconos/add.png")));
+		
+		btnNewButton_2 = new JButton("Actualizar");
+		btnNewButton_2.setBounds(10, 95, 128, 32);
+		panel_1.add(btnNewButton_2);
+		btnNewButton_2.setIcon(new ImageIcon(JdCama.class.getResource("/iconos/edit.png")));
+		
+		btnNewButton_3 = new JButton("Eliminar");
+		btnNewButton_3.setBounds(10, 138, 128, 32);
+		panel_1.add(btnNewButton_3);
+		btnNewButton_3.setIcon(new ImageIcon(JdCama.class.getResource("/iconos/delete.png")));
+		
+		btnNewButton_4 = new JButton("Cancelar");
+		btnNewButton_4.setBounds(10, 179, 128, 32);
+		panel_1.add(btnNewButton_4);
+		btnNewButton_4.setIcon(new ImageIcon(JdCama.class.getResource("/iconos/cancelar.png")));
+		
 		btnNewButton = new JButton("Nuevo");
+		btnNewButton.setBounds(10, 11, 128, 30);
+		panel_1.add(btnNewButton);
 		btnNewButton.setIcon(new ImageIcon(JdCama.class.getResource("/iconos/new.png")));
+		
+		panel_2 = new JPanel();
+		panel_2.setBackground(new Color(173, 216, 230));
+		panel_2.setBounds(29, 223, 396, 49);
+		contentPanel.add(panel_2);
+		panel_2.setLayout(null);
+		
+		btnNewButton_6 = new JButton("Buscar");
+		btnNewButton_6.setBounds(10, 11, 111, 32);
+		panel_2.add(btnNewButton_6);
+		btnNewButton_6.setIcon(new ImageIcon(JdCama.class.getResource("/iconos/search.png")));
+		
+		btnNewButton_5 = new JButton("Salir");
+		btnNewButton_5.setBounds(288, 12, 98, 30);
+		panel_2.add(btnNewButton_5);
+		btnNewButton_5.setIcon(new ImageIcon(JdCama.class.getResource("/iconos/exit.gif")));
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				actionPerformedBtnNewButton(arg0);
 			}
 		});
-		btnNewButton.setBounds(447, 53, 128, 30);
-		contentPanel.add(btnNewButton);
-		
-		cboIngresar = new JButton("Agregar");
-		cboIngresar.setIcon(new ImageIcon(JdCama.class.getResource("/iconos/add.png")));
 		cboIngresar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				actionPerformedCboIngresar(e);
 			}
 		});
-		cboIngresar.setBounds(447, 94, 128, 32);
-		contentPanel.add(cboIngresar);
-		
-		btnNewButton_2 = new JButton("Actualizar");
-		btnNewButton_2.setIcon(new ImageIcon(JdCama.class.getResource("/iconos/edit.png")));
-		btnNewButton_2.setBounds(447, 137, 128, 32);
-		contentPanel.add(btnNewButton_2);
-		
-		btnNewButton_3 = new JButton("Eliminar");
-		btnNewButton_3.setIcon(new ImageIcon(JdCama.class.getResource("/iconos/delete.png")));
-		btnNewButton_3.setBounds(447, 180, 128, 32);
-		contentPanel.add(btnNewButton_3);
-		
-		btnNewButton_4 = new JButton("Cancelar");
-		btnNewButton_4.setIcon(new ImageIcon(JdCama.class.getResource("/iconos/cancelar.png")));
-		btnNewButton_4.setBounds(447, 221, 128, 32);
-		contentPanel.add(btnNewButton_4);
-		
-		btnNewButton_5 = new JButton("Salir");
-		btnNewButton_5.setBounds(336, 230, 89, 23);
-		contentPanel.add(btnNewButton_5);
-		
-		btnNewButton_6 = new JButton("Buscar");
-		btnNewButton_6.setIcon(new ImageIcon(JdCama.class.getResource("/iconos/search.png")));
-		btnNewButton_6.setBounds(30, 225, 111, 32);
-		contentPanel.add(btnNewButton_6);
-		
-		scrollPane = new JScrollPane();
-		scrollPane.setBounds(30, 268, 545, 130);
-		contentPanel.add(scrollPane);
 		
 		llenandoComboCategoria();
 		llenandoComboEstado();
+		
+		mostrarListado();
 	}
+	
+	private void mostrarListado() {
+		// TODO Auto-generated method stub
+		inicializaTabla();
+		
+		for (int i = 0; i < camas.tamano(); i++) {
+			Cama cama = camas.obtener(i);
+			//Hallando la categoria
+			int indiceCategoria = cama.getCategoria();
+			String categoria = "";
+			
+			if(indiceCategoria == 0) categoria = "Economico";
+			else categoria = "Ejecutivo";
+			//Hallando el estado
+			int indiceEstado = cama.getEstado();
+			String estado = "";
+			
+			if(indiceEstado == 0) estado = "Libre";
+			else estado = "Desocupado";
+			
+			Object data[] = {cama.getNumero(),categoria, cama.getPrecioDia(), estado};
+			modeloCama.addRow(data);
+		}
+	}
+
+	private void inicializaTabla() {
+		// TODO Auto-generated method stub
+		int filas = tblCamas.getRowCount();
+		for (int i = 0; i < filas; i++) {
+			modeloCama.removeRow(0);
+		}
+	}
+
 	private void llenandoComboEstado() {
 		// TODO Auto-generated method stub
 		cboEstado.addItem("Libre");
@@ -186,7 +253,7 @@ public class JdCama extends JDialog {
 	}
 
 	protected void actionPerformedBtnNewButton(ActionEvent arg0) {
-		txtNumero.setText(Integer.toString(camas.generarCodigo()));
+		lblCodigo.setText(Integer.toString(camas.generarCodigo()));
 	}
 	protected void actionPerformedCboIngresar(ActionEvent e) {
 		
@@ -200,6 +267,7 @@ public class JdCama extends JDialog {
 			Cama cama= new Cama(leerNumeroCama(), leerCategoria(), leerPrecio(), leerEstado());
 			camas.adicionar(cama);
 			mensaje("Registro ingresado correctamente");
+			mostrarListado();
 			camas.grabar(); //Grabamos en el archivo de texto
 		} catch (Exception e) {
 			// TODO: handle exception
@@ -230,7 +298,7 @@ public class JdCama extends JDialog {
 
 	private int leerNumeroCama() {
 		// TODO Auto-generated method stub
-		return Integer.parseInt(txtNumero.getText());
+		return Integer.parseInt(lblCodigo.getText());
 	}
 }
 
